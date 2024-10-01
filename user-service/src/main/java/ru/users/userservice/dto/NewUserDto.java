c269a9aabff69f88a9cda6e7afe3f42ef6b347ed
Egor <alexanoffegor@yandex.ru>
2024-10-01 19:55:35 +0300

    refactor: add @Schema to dto

--- user-service/src/main/java/ru/users/userservice/dto/NewUserDto.java
+++ user-service/src/main/java/ru/users/userservice/dto/NewUserDto.java
@@ -2,6 +2,7 @@ package ru.users.userservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -18,14 +19,17 @@ public class NewUserDto {
 
     @NotBlank
     @Size(max = 256, message = "Invalid request format")
+    @Schema(description = "Имя пользователя")
     private String name;
 
     @NotBlank
     @Size(max = 256, message = "Invalid request format")
+    @Schema(description = "Фамилия пользователя")
     private String surname;
 
     @NotNull(message = "Invalid request format")
     @JsonFormat(pattern = "yyyy-MM-dd")
     @JsonProperty("registration_date")
+    @Schema(description = "Дата регистрации пользователя (в формате yyyy-MM-dd)")
     private LocalDate registrationDate;
 }
--- user-service/src/main/java/ru/users/userservice/dto/UpdateUserDto.java
+++ user-service/src/main/java/ru/users/userservice/dto/UpdateUserDto.java
@@ -2,6 +2,7 @@ package ru.users.userservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,12 +14,15 @@ import java.time.LocalDate;
 public class UpdateUserDto {
 
     @Size(max = 256, message = "Invalid request format")
+    @Schema(description = "Имя пользователя", maxLength = 256)
     private String name;
 
     @Size(max = 256, message = "Invalid request format")
+    @Schema(description = "Фамилия пользователя", maxLength = 256)
     private String surname;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
     @JsonProperty("registration_date")
+    @Schema(description = "Дата регистрации пользователя (в формате yyyy-MM-dd)")
     private LocalDate registrationDate;
 }
